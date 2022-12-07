7a5604db7472f91fd790856e7f76357f7d69ec29
jo3h4rk3r <66574390+jo3h4rk3r@users.noreply.github.com>
2022-12-07 14:24:54 -0500

    Updated to 1.19.3

--- src/main/java/unlimited/litehacks/gui/ui.java
+++ src/main/java/unlimited/litehacks/gui/ui.java
@@ -1,11 +1,13 @@
 package unlimited.litehacks.gui;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.GameModeSelectionScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.option.OptionsScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
+import unlimited.litehacks.gui.clickgui.ClickGui;
 
 import java.awt.*;
 
@@ -16,16 +18,26 @@ public class ui extends Screen {
         super(Text.translatable("user interface screen"));
     }
 
+
+
+
     public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
         drawCenteredText(matrices, mc.textRenderer, Text.literal("Test"), mouseX,mouseY, Color.WHITE.getRGB());
 
-    //    int l = this.height / 4 + 48;
-        this.addDrawableChild(new ButtonWidget(this.width / 2 - 100, 1 + 72 + 12, 98, 20, Text.translatable("menu.options"), (button) -> {
+
+
+        this.addDrawableChild(ButtonWidget.builder(Text.translatable("menu.options"), (button) -> {
             assert this.client != null;
             this.client.setScreen(new OptionsScreen(this, this.client.options));
-        }));
+        }).dimensions(this.width / 2 - 100, 1 + 72 + 12, 98, 20).build());
 
 
     }
+
+
+
+
+
+
 
 }
--- src/main/java/unlimited/litehacks/mixin/MixinTitleScreen.java
+++ src/main/java/unlimited/litehacks/mixin/MixinTitleScreen.java
@@ -49,4 +49,5 @@ public class MixinTitleScreen extends Screen {
 
 
 
+
 }
